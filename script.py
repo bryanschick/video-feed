6ace5f86384357b22d04f86d21c909e335fb1dc7
bryanschick <124395110+bryanschick@users.noreply.github.com>
2023-05-14 17:31:48 -0400

    Update script.py

--- script.py
+++ script.py
@@ -14,14 +14,10 @@ feeds = [
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCm8xNi3kuBHE99QH-N_VJVg", #sharmeleon
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCLXo7UDZvByw2ixzpQCufnA", #vox
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCmGSJVG3mCRXVOP4yZrU1Dw", #johnny harris
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UC415bOPUcGSamy543abLmRA", #cleo abram
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCZB6V9fUov0Mx_us3MWWILg", #people make games
     "https://www.youtube.com/feeds/videos.xml?channel_id=UC0fDG3byEcMtbOqPMymDNbw", #noclip
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCR9ygnRBOt8qfSbkCwOA3pw", #samurai matcha
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCQLq3ElThYSwaO2U9Sz8Jow", #here's my question for you
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UCJ24N4O0bP7LGLBDvye7oCA", #matt d'avella
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UCrdWRLq10OHuy7HmSckV3Vg", #nathaniel drew
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UCZbOmMsPgxuWmj5pGymAOnA", #nathaniel drew no backup
     ]
 
 class VideoEntry:
@@ -71,7 +67,7 @@ def feedparser_to_local_domain(feedUri, recent_datetime):
     
 #todo: real templating
 def render(videoEntries, build_datetime):
-    html = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="robots" content="noindex, nofollow"><title>Video Feed: '+str(build_datetime)+'</title><style>.entries {display:flex;flex-wrap:wrap;justify-content:center} .entry {flex: 0 1 40ch;margin:1ch} .thumbnail {width:100%}</style></head><body><div class="entries">'
+    html = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="robots" content="noindex, nofollow"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Video Feed: '+str(build_datetime)+'</title><style>.entries {display:flex;flex-wrap:wrap;justify-content:center} .entry {flex: 0 1 40ch;margin:1ch} .thumbnail {width:100%}</style></head><body><div class="entries">'
     for video in videoEntries:
         html += '<div class="entry">'
         html += '<img class="thumbnail" src="'+video.altThumbnail()+'">'
